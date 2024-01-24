7f4f83f3623ff0e3d0e38b9074918f51f9df15d0
PavanButke <butkepavan55@gmail.com>
2024-01-24 22:53:19 +0530

    sample client-server using ws

--- GolangBasics/main.go
+++ GolangBasics/main.go
@@ -2,83 +2,39 @@ package main
 
 import (
 	"fmt"
- //"reflect"
+	"github.com/gorilla/websocket"
+	"net/http"
 )
 
-const(
-	Name = "Pavan"
-	DOB  = "1307"
-	TYPE = "Sapien"
-)
-func main()  {	
+func main() {
+	// WebSocket server URL
+	//serverURL := "ws://server.example.com/chat"
+	serverURL := "wss://echo.websocket.org"
 
-	var a=12 
-	var ptr1 *int
+	// Additional WebSocket headers
+	headers := http.Header{
+		"Upgrade":                  {"websocket"},
+		"Connection":               {"Upgrade"},
+		"Sec-WebSocket-Key":        {"dGhlIHNhbXBsZSBub25jZQ=="},
+		"Origin":                   {"http://example.com"},
+		"Sec-WebSocket-Protocol":   {"chat, superchat"},
+		"Sec-WebSocket-Version":    {"13"},
+	}
 
-	var b=21
-	var ptr2 *int
+	// Establish WebSocket connection
+	conn, _, err := websocket.DefaultDialer.Dial(serverURL, headers)
+	if err != nil {
+		fmt.Println("WebSocket connection error:", err)
+		return
+	}
+	defer conn.Close()
 
-	ptr1 = &a
-	ptr2 = &b 
+	fmt.Println("WebSocket handshake successful!")
 
-	fmt.Printf("This is the value %v and %v", a , b)
-	fmt.Printf("\nThis is the  adddress of %v and %v", &a , &b)
-	fmt.Printf("\nThis is the  adddress of %v and %v", ptr1 , ptr2)
-	fmt.Printf("\nThis is the  values of  a and by ptr1 & ptr2 %v and %v", *ptr1 , *ptr2)
+	// Your WebSocket communication logic goes here
+	// For example, you can send and receive messages using conn.WriteMessage and conn.ReadMessage
 
-
-	c:= *ptr1 + *ptr2
-	fmt.Println("\nThe sum of values  ",c)
-
-	// var a interface{}
-	// a="This a data itself."
-
-	// switch t:= a.(type) {
-	// case int64:
-	// 	fmt.Println("Type is an integer: ", t )
-	
-	// case string:
-	// 	fmt.Println("Type is an string: ", t )
-
-	// case bool:
-	// 	fmt.Println("Type is an boolean: ", t )
-	
-	// default:
-	// 	fmt.Println("Idk , the data type!")
-	// }
-	// fmt.Println("Hello Computers")
-	// var x int
-	// var y =10
-	// z:=10
-
-	// fmt.Printf("res: x is typeof %v and y is type of %v and z is type of %v\n",
-	// reflect.TypeOf(x),
-	// reflect.TypeOf(y),
-	// reflect.TypeOf(z))
-	// test()
-
-	// fmt.Println(sum(x,y,z))
-	// scanOps()
-
-}	
-
-// func test(){
-// 	fmt.Println("Testing started!")
-// }
-// func sum(num1 int , num2 int , num3 int) (int, int , int){
-// 	res := num1 +num2 +num3 
-// 	return res,0,0;
-// }
-
-// func scanOps(){
-// 	var userInput string
-// 	fmt.Println("Please enter Age: ")
-// 	fmt.Scan(&userInput)
-// 	fmt.Printf("User %s is born on %s and his age is %s of type %s",
-// 		Name,
-// 		DOB,
-// 		userInput,
-// 		TYPE,
-		
-// 	)
-// }
+	// Here, we'll just wait for user input to keep the program running
+	fmt.Println("Press Enter to exit.")
+	fmt.Scanln()
+}
