14561ba432c14840cc5cc24c682b3fbd06423bb6
PavanButke <butkepavan55@gmail.com>
2024-01-24 23:40:15 +0530

    chatapp

---
+++ GolangBasics/ChatApp/client_ws.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"os"
+	"os/signal"
+	"time"
+)
+
+type client struct{
+	//web socket
+	socket *websocket.Conn
+	//channel to recieve msg from others 
+	receive chan []byte
+	//chat room
+	room *room
+}
+
+func (c* client) read(){
+	//close if we have read
+	defer c.socket.Close()
+
+		for {
+			_, msg, err := s.socket.ReadMessage()
+			if err != nil{
+				return
+			}
+			//msg recieving from channel
+			c.room.forward <- msg
+		}
+
+}
+
+
+func main(){
+
+}
